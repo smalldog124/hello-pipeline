22b8d1f9f905428a798941a15f5933a2e45b3516
smalldog124 <panumars124@gmail.com>
2019-08-11 15:25:34 +0700

    [Edited]:แก้ไขให้เรียกใช้ service ที่จะไป call api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HelloService } from './reques-api/hello.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title : string;
+  constructor(public api:HelloService){}
+
+  ngOnInit(): void {
+    this.getMessage()
+  }
+
+  getMessage(){
+    return this.api.getHello().subscribe((data: any)=>{
+      this.title = data.message;
+    });
+  }
 }
