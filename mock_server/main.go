803142685e23447eac5f20695220e1b43e3753a6
smalldog124 <panumars124@gmail.com>
2019-08-11 15:21:01 +0700

    [Edited]:แก้ไข AllowOrigins ให้หน้าบ้าน

--- mock_server/main.go
+++ mock_server/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+	config := cors.New(
+		cors.Config{
+			AllowOrigins: []string{"http://localhost:4200"},
+		},
+	)
+	r.Use(config)
 	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "สวัสดี ชาวโลก",
