5e7a05e8b079877c0b282b3b368948493f1e6cc7
smalldog124 <panumars124@gmail.com>
2019-08-11 16:33:02 +0700

    [Added]:เพิ่มเทสของ service ของ hello

--- src/app/reques-api/hello.service.spec.ts
+++ src/app/reques-api/hello.service.spec.ts
@@ -1,12 +1,42 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HelloService } from './hello.service';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { HelloMessage } from './hello-message';
 
 describe('HelloService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: HelloService
+  let httpTestingController: HttpTestingController
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HelloService]
+    })
+    service = TestBed.get(HelloService);
+    httpTestingController = TestBed.get(HttpTestingController)
+  });
 
   it('should be created', () => {
     const service: HelloService = TestBed.get(HelloService);
     expect(service).toBeTruthy();
+  });
+
+  it('Service testing with HTTP client', () => {
+    // Arrange
+    const response = {
+      message: 'Dummy'
+    };
+
+    // Act and assert
+    service.getHello().subscribe((data: HelloMessage) => {
+      expect(data.message).toEqual('Dummy');
+    });
+
+    // Assert
+    const request = httpTestingController.expectOne('http://localhost:3000/hello');
+    expect(request.request.method).toEqual('GET');
+    request.flush(response); 
+    httpTestingController.verify();
   });
 });
