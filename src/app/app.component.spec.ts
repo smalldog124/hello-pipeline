a7b1172898eab9c7abc7d809d9353f0cb885fbf2
smalldog124 <panumars124@gmail.com>
2019-08-11 16:25:48 +0700

    [Added]:เพิ่มเทสของ app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,59 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async,ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing'
+import { HelloService } from './reques-api/hello.service';
+import { HelloMessage } from './reques-api/hello-message';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let service: HelloService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
       ],
+      providers:[
+        HelloService
+      ]
     }).compileComponents();
+        // create component and test fixture
+        fixture = TestBed.createComponent(AppComponent);
+
+        // get test component from the fixture
+        component = fixture.componentInstance;
+    
+        // UserService provided to the TestBed
+        service = TestBed.get(HelloService);
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('frontend');
-  });
+  it('should render title in a h1 tag', fakeAsync(() => {
+    // Arrange
+    const dummy = new HelloMessage();
+    dummy.message = "dummy";
+    spyOn(service,'getHello').and.returnValue(of(dummy));
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+    // Act
+    component.ngOnInit();
+
+    // Assert
+    tick();
+    fixture.detectChanges()
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to frontend!');
-  });
+    expect(compiled.querySelector('h1').textContent).toContain('dummy');
+  })
+  );
 });
